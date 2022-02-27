93c536248ecf13d28fc59ce8b3feb2c6166534d1
Danny Robinson <dannyrobinson@gmx.com>
2022-02-27 04:01:07 +0000

    Deque

---
+++ deque.lua
@@ -0,0 +1,115 @@
+---@class Deque
+---A deque is a Double-Ended QUEue.
+local Deque = require("class")()
+
+---Create a new deque from a list. Creates an empty deque if no list is passed.
+---@param list table
+---@return Deque
+function Deque:new(list)
+    local deque = list and { table.unpack(list) } or {}
+    setmetatable(deque, self)
+    self.__index = self
+    return deque
+end
+
+---Append the other deque to the end of the deque.
+---@param other Deque
+---@return Deque self
+function Deque:append(other)
+    table.move(other, 1, #other, #self + 1, self)
+    return self
+end
+
+---Create a shallow copy of the deque.
+function Deque:copy()
+    return Deque:new(self)
+end
+
+---Empty the deque, reseting it to its initial state.
+function Deque:empty()
+    for index, _ in ipairs(self) do
+        self[index] = nil
+    end
+    return self
+end
+
+function Deque:isempty()
+    return self[1] == nil
+end
+
+---Remove a value from the end of the deque.
+function Deque:pop()
+    if self:isempty() then
+        error "Deque is empty"
+    end
+    return table.remove(self)
+end
+
+---Remove a value from the start of the deque.
+function Deque:popfirst()
+    if self:isempty() then
+        error "Deque is empty"
+    end
+    return table.remove(self, 1)
+end
+
+---Prepend the other deque to the start of the deque.
+---@param other Deque
+---@return Deque self
+function Deque:prepend(other)
+    table.move(self, 1, #self, #other + 1)
+    table.move(other, 1, #other, 1, self)
+    return self
+end
+
+---Add a value to the end of the deque.
+---@param value any
+---@return Deque self
+function Deque:push(value)
+    table.insert(self, value)
+    return self
+end
+
+---Add a value to the start of the deque.
+---@param value any
+---@return Deque self
+function Deque:pushfirst(value)
+    table.insert(self, 1, value)
+    return self
+end
+
+---Reverse the deque in place.
+---@return Deque self
+function Deque:reverse()
+    for low = 1, #self // 2 do
+        local high = #self - low + 1
+        self[low], self[high] = self[high], self[low]
+    end
+    return self
+end
+
+Deque.shift = Deque.popfirst
+
+Deque.unshift = Deque.pushfirst
+
+---@param other Deque
+function Deque:__eq(other)
+    if #self ~= #other then
+        return false
+    end
+    for index, value in ipairs(self) do
+        if other[index] ~= value then
+            return false
+        end
+    end
+    return true
+end
+
+Deque.__name = "Deque"
+
+---@return string
+function Deque:__tostring()
+    return self.__name .. "{" .. table.concat(self, ", ") .. "}"
+end
+
+return Deque
---
+++ deque.test.lua
@@ -0,0 +1,71 @@
+local Deque = require "deque"
+
+local deque = Deque:new()
+
+assert(#deque == 0, "New Deque should have length 0.")
+deque:push(42)
+assert(#deque == 1, "Deque should have length 1.")
+assert(deque:pop() == 42, "Deque should be able to pop.")
+
+local other = Deque()
+assert(deque == other, "Empty deques should be equal.")
+
+deque:push(13)
+deque:push(14)
+assert(#deque == 2, "Deque should have length 2.")
+assert(deque:pop() == 14, "Deque should be able to pop items in order.")
+assert(deque:pop() == 13, "Deque should be able to pop items in order.")
+
+deque:push(1)
+other:push(1)
+assert(deque == other, "Non-empty deques should be equal.")
+
+other:push(2)
+assert(deque ~= other,
+    "Different length deques with equal values in the overlapping region should not be equal.")
+deque:push(3)
+assert(deque ~= other,
+    "Matching length deques with different elements should not be equal.")
+
+deque:empty()
+assert(#deque == 0, "Emptied deque should have length 0.")
+
+deque:push(15)
+deque:push(16)
+deque:push(17)
+deque:pushfirst(14)
+deque:pushfirst(13)
+assert(#deque == 5, "Deque should have length 5.")
+assert(deque:popfirst() == 13, "Deque should be able to pop items in order.")
+assert(deque:pop() == 17, "Deque should be able to pop items in order.")
+assert(deque:pop() == 16, "Deque should be able to pop items in order.")
+assert(deque:popfirst() == 14, "Deque should be able to pop items in order.")
+assert(deque:popfirst() == 15, "Deque should be able to pop items in order.")
+
+deque:push(1)
+deque:push(2)
+deque:push(3)
+deque:push(4)
+assert(table.concat(deque, ",") == "1,2,3,4",
+    "Should be able to table.concat a deque.")
+deque:reverse()
+assert(deque == Deque { 4, 3, 2, 1 }, "Should be able to reverse a deque.")
+deque:pushfirst(5)
+deque:reverse()
+assert(deque == Deque { 1, 2, 3, 4, 5 },
+    "Should be able to reverse a deque with low firstindex.")
+
+other = Deque { 1, 2, 3, 4, 5 }
+assert(other == deque, "Should be able to construct a deque from a list.")
+
+deque:append(Deque { 6, 7, 8, 9, 10 })
+assert(#deque == 10, "Deque should have length 10.")
+assert(deque == Deque { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+    "Should be able to append to the end of deque.")
+
+other = Deque { -4, -3, -2, -1, 0 }
+deque:prepend(other)
+assert(deque == Deque { -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
+    "Should be able to prepend to the start of a deque.")
+
+print("deque.lua - Tests passed.")
